860b76fd4a620917c246d05c7ed70241a75fbbf5
unknown <ivan.lomando@ucu.edu.uy>
2022-08-22 20:47:32 -0300

    prueba2

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿class producto {
+    string nombre;
+    int precio;
+    List<string> review;
+public producto (string nombre, int precio, List<string> review) {
+        
+            this.nombre = nombre;
+        
+            this.precio = precio;
+
+            this.review = review;
+}
+public override string ToString(){
+        {
+             return $" el nombre es:{this.nombre} el precio es {this.precio} y las valoraciones {this.review}";
+        }   
+
+        }
+
+
+
+public void agregarreview(review){
+
+
+
+
+}
+
+}
+
+class review {
+
+int clasificacion;
+
+string comentarios;
+
+public review ( int clasificacion, string comentarios) {
+        
+this.clasificacion = clasificacion;
+this.comentarios = comentarios;
+
+}
+}
